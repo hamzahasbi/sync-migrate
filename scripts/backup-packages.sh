0e7029e6d98b08fd507b4210f7610c660bbc658a
hamzahasbi <hamza.hasbi@gmail.com>
2021-09-03 20:37:59 +0100

    Fix: file architecture

--- scripts/backup-packages.sh
+++ scripts/backup-packages.sh
@@ -1,6 +1,6 @@
-composer global show -i > composer.txt
-npm list -g --depth=0 > node.txt
-yarn global list --depth=0 >> node.txt
+composer global show -i > ./packages/composer.txt
+npm list -g --depth=0 > ./packages/node.txt
+yarn global list --depth=0 >> ./packages/node.txt
 brew list > brew.txt
-ruby -S gem list --local > gems.txt
+ruby -S gem list --local > ./packages/gems.txt
 
